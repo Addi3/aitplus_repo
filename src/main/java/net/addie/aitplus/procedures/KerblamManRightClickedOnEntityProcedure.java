e7275c8c2678a7c8f4726d6db8eb26099c7bfd08
Addi3 <addiec787@gmail.com>
2024-12-15 21:18:40 +0000

    kerblam emeralds can now accept >  or = to 5 emeralds

--- src/main/java/net/addie/aitplus/procedures/KerblamManRightClickedOnEntityProcedure.java
+++ src/main/java/net/addie/aitplus/procedures/KerblamManRightClickedOnEntityProcedure.java
@@ -25,7 +25,7 @@ public class KerblamManRightClickedOnEntityProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
-		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.EMERALD && (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getCount() == 5) {
+		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.EMERALD && (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getCount() >= 5) {
 			if (world instanceof ServerLevel _level) {
 				ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(AitplusModBlocks.KERBLAM_BOX));
 				entityToSpawn.setPickUpDelay(10);
